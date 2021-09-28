4ff7cc824e7b1f585c63a73ab09d63db55f2c7de
leeharim <idegami1559@icloud.com>
2021-09-28 19:00:27 +0900

    project init

---
+++ server.js
@@ -0,0 +1,31 @@
+import express from 'express';
+import bodyParser from 'body-parser';
+import morgan from 'morgan';
+import cors from 'cors';
+import colors from 'colors';
+
+import dotEnv from 'dotenv';
+import { errorHandler, notFound } from './middleware/errMiddleware.js';
+
+dotEnv.config();
+
+const app = express()
+
+
+// middleware
+app.use(cors())
+app.use(morgan('dev'))
+app.use(bodyParser.json())
+app.use(bodyParser.urlencoded({extended: false}))
+
+
+// global error handler
+app.use(notFound)
+app.use(errorHandler)
+
+
+const PORT = process.env.PORT || 3000;
+
+app.listen(PORT, () => {
+    console.log(`Server running in ${process.env.PORT} mode on port ${PORT}`.yellow.bold)
+})
