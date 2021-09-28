87f175471909cf7a5bdb321bb3998969da59f86a
leeharim <idegami1559@icloud.com>
2021-09-28 19:44:09 +0900

    add product model

---
+++ models/productModel.js
@@ -0,0 +1,77 @@
+import mongoose from 'mongoose';
+const { Schema, model } = mongoose;
+
+
+const reviewSchema = Schema(
+    {
+        name: {type: String, required: true},
+        rating: {type: Number, required: true},
+        comment: {type: String, required: true},
+        user: {
+            type: Schema.Types.ObjectId,
+            required: true,
+            ref: 'user'
+        }
+    },
+    {
+        timestamps: true
+    }
+)
+
+const productSchema = Schema(
+    {
+        user: {
+            type: Schema.Types.ObjectId,
+            required: true,
+            ref: "user"
+        },
+        name: {
+            type: String,
+            required: true
+        },
+        image: {
+            type: String,
+            required: true,
+        },
+        brand: {
+            type: String,
+            required: true
+        },
+        category: {
+            type: String,
+            required: true
+        },
+        description: {
+            type: String,
+            required: true
+        },
+        reviews: [reviewSchema],
+        rating: {
+            type: Number,
+            required: true,
+            default: 0
+        },
+        numReviews: {
+            type: Number,
+            required: true,
+            default: 0
+        },
+        price: {
+            type: Number,
+            required: true,
+            default: 0
+        },
+        countInStock: {
+            type: Number,
+            required: true,
+            default: 0
+        }
+    },
+    {
+        timestamps: true
+    }
+)
+
+const Product = model('product', productSchema);
+
+export default Product
